983da0bb7d01f0e78c8c469b5488ce5dfe6f0770
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2025-02-03 17:18:22 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,27 @@
+public class ArithmeticOperators {
+        public static void main(String[] args) {
+        // Declare two variables
+        int a = 10;
+        int b = 5;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus (remainder)
+        int remainder = a % b;
+        System.out.println("Remainder: " + remainder);
+    }
+}
