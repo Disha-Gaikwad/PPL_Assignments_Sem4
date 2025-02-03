4b03c3c26547f4e6fadc2ec3be35b465929ed45d
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2025-02-03 17:17:51 +0530

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,23 @@
+# Declare two variables
+a = 10
+b = 5
+
+# Addition
+sum_result = a + b
+print("Sum:", sum_result)
+
+# Subtraction
+difference = a - b
+print("Difference:", difference)
+
+# Multiplication
+product = a * b
+print("Product:", product)
+
+# Division
+quotient = a / b
+print("Quotient:", quotient)
+
+# Modulus (remainder)
+remainder = a % b
+print("Remainder:", remainder)
